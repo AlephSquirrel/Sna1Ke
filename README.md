ae51c0b936012e13f7a9ff5b8631e2ed8d7600bf
AlephSquirrel <33697577+AlephSquirrel@users.noreply.github.com>
2025-08-17 16:59:44 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sna1Ke
+A Snake game written in only 1000 bytes of Python code, using the [Pygame](pygame.org) library.
+Features:
+ - 32x32 toroidal grid
+ - Ignores arrow key presses opposite to the direction of the snake's head
+ - Kill screen (shows your final score briefly, then restarts)
---
+++ Sna1Ke.py
@@ -0,0 +1,23 @@
+#by AlephSquirrel
+import pygame as P
+from random import randrange as R
+P.init()
+C=P.time.Clock()
+D=P.display.set_mode((640,)*2)
+F=P.font.SysFont(0,24)
+U=P.display.update
+P.display.set_caption('Sna1Ke')
+while 1:
+ x=y=d=2;s=0;l=[];f=R(32);g=R(32)
+ while C.tick(15):
+  for e in P.event.get():
+   e.type==12>exit()
+   if e.type==2<272<e.key<277:k=e.key%7;d=k^(k==d^1)
+  x=x+d%3+d%~d&31;y=y+2-5//~d%4&31
+  if(x-f|y-g)==0:s+=1;f=R(32);g=R(32)
+  else:l=l[len(l)>2:]
+  if(x,y)in l:break
+  l+=(x,y),;D.fill((0,)*3)
+  for i in range(32):P.draw.line(D,(60,)*3,(0,i*20),(640,i*20));P.draw.line(D,(60,)*3,(i*20,0),(i*20,640))
+  [P.draw.rect(D,(0,180+15*i,0),P.Rect(X*20+i,Y*20+i,20-2*i,20-2*i))for X,Y in l for i in(0,4)];P.draw.circle(D,(255,0,0),(f*20+11,g*20+11),9);P.draw.line(D,(0,240,0),(f*20+10,g*20+3),(f*20+18,g*20+2),3);t=F.render(f"Score: {s}",1,(255,)*3);D.blit(t,t.get_rect());U()
+ t=F.render(f"Game over! Score: {s}",1,(255,)*3);r=t.get_rect();r.center=D.get_rect().center;D.blit(t,r);U();C.tick(.4)
